ae929469171f4842bc098eb283469a337bc4dbed
lewhub <bracey.lewis@gmail.com>
2016-03-04 15:13:38 -0800

    done meme model ref is Celebrity

--- models/Meme.js
+++ models/Meme.js
@@ -1,1 +1,11 @@
 var mongoose = require('mongoose')
+
+var memeSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  url: {type: String, unique: true, required: true},
+  _by: {type: mongoose.Schema.Types.ObjextId, ref: 'Celebrity'}
+})
+
+var Meme = mongoose.model('Meme', memeSchema)
+
+module.exports = Meme
