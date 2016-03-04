94501db0ca97b15638e34b6e05adec88ae229141
Bryan Castanos <bcastanos@gmail.com>
2016-03-04 15:30:05 -0800

    comment about different styles

--- models/Celebrity.js
+++ models/Celebrity.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose')
 
 var celebritySchema = new mongoose.Schema({
   name: {type: String, required: true},
+  //let's test this and if it doesn't work we can switch to [memeSchema]
   memes: [{type: mongoose.Schema.Types.ObjectId, ref: "Meme"}]
 })
 
